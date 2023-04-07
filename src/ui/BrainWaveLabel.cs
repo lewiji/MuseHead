aea2dd774c3b33bf43428aa8e4533b1f867ed1ed
Lewis James <lewiji+github@gmail.com>
2023-04-07 20:38:54 +0100

    Tweak 3d physics params & add Area3D gravity override in concentric spheres for control purposes. Implement dominant frequency band label

--- src/ui/BrainWaveLabel.cs
+++ src/ui/BrainWaveLabel.cs
@@ -1,10 +1,31 @@
 using Godot;
 using System;
+using System.Linq;
+using Godot.Collections;
+using MuseHead;
+using MuseHead.eeg;
 
 public partial class BrainWaveLabel : Label
 {
-	// Called when the node enters the scene tree for the first time.
+	int _currMaxIdx;
 	public override void _Ready()
 	{
+		GetNode<MuseConnector>("/root/MuseConnector").EegReceived += OnEegReceived;
+	}
+
+	void OnEegReceived(Dictionary<int, double[]> data)
+	{
+		var max = 0.0;
+		var maxIdx = 0;
+		for (var i = 0; i < data[-1].Length; i++)
+		{
+			if ((data[-1][i] <= max)) continue;
+			max = data[-1][i];
+			maxIdx = i;
+		}
+
+		if (maxIdx == _currMaxIdx) return;
+		_currMaxIdx = maxIdx;
+		Text = ((BrainWave)maxIdx).ToString();
 	}
 }
